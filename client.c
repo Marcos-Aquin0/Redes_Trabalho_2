cf5fce74940b9fd9267e7acffc856900662e0312
Marcos Aquino <marcos.aquino@unifesp.br>
2025-10-29 22:29:30 -0300

    arquivos iniciais

---
+++ client.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+        if (argc !=3) {
+                printf("Usage: %s <dst_ip> <dst_port>\n", argv[0]);
+                return 0;
+        }
+        // socket
+        int cfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (cfd == -1) {
+                perror("socket()");
+                return -1;
+        }
+        struct sockaddr_in saddr;
+        saddr.sin_port = htons(atoi(argv[2]));
+        saddr.sin_family = AF_INET;
+        saddr.sin_addr.s_addr = inet_addr(argv[1]);
+
+        // connet
+        if (connect(cfd, (struct sockaddr*) &saddr, sizeof(struct sockaddr_in)) == -1) {
+                perror("connect()");
+                return -1;
+        }
+        #define MAX_MSG 256
+        char msg[MAX_MSG], resp[MAX_MSG];
+        bzero(msg, MAX_MSG);
+        printf("<telnet teletype>: ");
+        fgets(msg, MAX_MSG, stdin);
+        int ns, nr;
+        // write
+        ns = write(cfd, msg, strlen(msg));
+        if (ns == -1) {
+                perror("write()");
+                return -1;
+        }
+        // read
+	bzero(resp, MAX_MSG);
+        nr = read(cfd, resp, MAX_MSG);
+        if (nr == -1) {
+                perror("read()");
+                return -1;
+        }
+	printf("\n%s", resp);
+        // close
+        close(cfd);
+        return 0;
+}
+
+
---
+++ server.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#define MAX_REQ 64536
+#define MAX_CONN 1000
+
+struct targs {
+	pthread_t tid;
+	int cfd;
+	struct sockaddr_in caddr;
+};
+typedef struct targs targs;
+
+targs tclients[MAX_CONN + 3];
+
+void init(targs *tclients, int n){
+	int i;
+	for (i = 0; i< MAX_CONN + 3; i++) {
+		tclients[i].cfd = -1;
+	}
+}
+
+void *handle_client(void *args) {
+		int cfd = *(int *)args;
+		// recv
+		int nr, ns;
+		unsigned char requisicao[MAX_REQ];
+		bzero(requisicao, MAX_REQ);
+		nr = recv(tclients[cfd].cfd, requisicao, MAX_REQ, 0);
+		if (nr < 0) {
+			perror("erro no recv()");
+			return NULL;
+		}
+
+		printf("Recebeu do cliente (%s:%d): '%s'\n", inet_ntoa(tclients[cfd].caddr.sin_addr),
+				ntohs(tclients[cfd].caddr.sin_port), requisicao);
+
+		// send
+		ns = send(cfd, requisicao, nr, 0);
+		if (ns < 0){
+			perror("erro no send()");
+			return NULL;
+		}
+		// close
+		close(tclients[cfd].cfd);
+
+		tclients[cfd].cfd = -1;
+		return NULL;
+}
+
+int main(int argc, char **argv) {
+
+	if (argc != 2){
+		printf("Uso: %s <porta> \n", argv[0]);
+		return 0;
+	}
+
+	init(tclients, MAX_CONN+3);
+
+	struct sockaddr_in saddr;
+
+	// socket
+	int sl = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	// bind
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons(atoi(argv[1]));
+	saddr.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind(sl, (const struct sockaddr *)&saddr, sizeof(struct sockaddr_in)) < 0) {
+		perror("erro no bind()");
+		return -1;
+	}
+
+	if (listen(sl, 1000) < 0) {
+		perror("erro no listen()");
+		return -1;
+	}
+
+	// accept
+	int cfd, addr_len, i;
+	struct sockaddr_in caddr;
+	addr_len = sizeof(struct sockaddr_in);
+
+	while(1) {
+
+		cfd = accept(sl, (struct sockaddr *)&caddr, (socklen_t *)&addr_len);
+		if (cfd == 1) {
+			perror("erro no accept()");
+			return -1;
+		}
+		tclients[cfd].cfd = cfd;
+		tclients[cfd].caddr = caddr;
+		pthread_create(&tclients[cfd].tid, NULL, handle_client, (void*)&cfd);
+	}
+	for (i = 0; i< MAX_CONN + 3; i++) {
+		if (tclients[i].cfd == -1) continue;
+		pthread_join(tclients[i].tid, NULL);
+	}
+	return 0;
+}
+ 

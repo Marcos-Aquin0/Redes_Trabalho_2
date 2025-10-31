06e8cd4a9ae62a0ec698969b9c0a92a321d0f0ab
tasso-canellas <tasso.canellas@unifesp.br>
2025-10-30 22:47:33 -0300

    deu green

--- server.c
+++ server.c
@@ -8,6 +8,7 @@
 #include <pthread.h>
 #include <string.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 
 #define MAX_REQ 64536
 #define MAX_CONN 1000
@@ -28,115 +29,124 @@ void init(targs *tclients, int n){
 	}
 }
 
+int compare(time_t file_time, time_t timestamp) {
+	return difftime(file_time, timestamp) >0;
+}
+
 void *handle_client(void *args) {
-		int cfd = *(int *)args;
-		// recv
-		int nr, ns;
-		char requisicao[MAX_REQ], resposta[MAX_REQ];
-		bzero(requisicao, MAX_REQ);
-		nr = recv(tclients[cfd].cfd, requisicao, MAX_REQ, 0);
-		if (nr < 0) {
-			perror("erro no recv()");
-			return NULL;
+	int cfd = *(int *)args;
+	// recv
+	int nr, ns;
+	char requisicao[MAX_REQ], resposta[MAX_REQ], arquivo[MAX_REQ] = "";
+	bzero(requisicao, MAX_REQ);
+	nr = recv(tclients[cfd].cfd, requisicao, MAX_REQ, 0);
+	if (nr < 0) {
+		perror("erro no recv()");
+		return NULL;
+	}
+
+	printf("Recebeu do cliente (%s:%d): '%s'\n", inet_ntoa(tclients[cfd].caddr.sin_addr),
+			ntohs(tclients[cfd].caddr.sin_port), requisicao);
+
+	const char delimiters[] = " \n";
+	char *command = strtok(requisicao, delimiters);
+	char *filename = strtok(NULL, delimiters);
+	char *timestamp = strtok(NULL, delimiters);
+
+	if (strcmp(command, "GET") == 0) {
+		int fd = open(filename, O_RDONLY);
+
+		if (fd == -1) {
+			strcpy(resposta, "404 Not Found\n");
+		} else {
+			struct stat st;
+			if (stat(filename, &st) == -1) {
+				perror("Erro ao obter stat");
+				strcpy(resposta, "500 Internal Server Error\n");
+				close(fd);
+			}
+			time_t ts = (time_t)atol(timestamp);
+			if (compare(st.st_mtime, ts)) {
+				sprintf(resposta, "304 Not Modified\n%ld\n", st.st_mtime);
+				close(fd);
+			} else {
+				char buffer[128];
+				ssize_t bytesLidos = read(fd, buffer, sizeof(buffer) - 1);
+				if (bytesLidos == -1) {
+					perror("Erro ao ler o arquivo");
+					strcpy(resposta, "500 Internal Server Error\n");
+					close(fd);
+					return NULL;
+				}
+				buffer[bytesLidos] = '\0';
+				strcpy(resposta, "200 OK\n");
+				strcpy(arquivo, buffer);
+				close(fd);
+			}
+		}
+	} else if (strcmp(command, "PUT") == 0) {
+		if (filename == NULL) {
+			strcpy(resposta, "400 Bad Request\n");
+		} else {
+			char old_filename[200] = "cliente/";
+			strcat(old_filename, filename);
+			char path_file[200] = "servidor/";
+			strcat(path_file, filename);
+			if (rename(old_filename, path_file) == 0) {
+				strcpy(resposta, "201 CREATED\n");
+			} else {
+				strcpy(resposta, "500 INTERNAL SERVER ERROR\n");
+			}
 		}
 
-		printf("Recebeu do cliente (%s:%d): '%s'\n", inet_ntoa(tclients[cfd].caddr.sin_addr),
-				ntohs(tclients[cfd].caddr.sin_port), requisicao);
+		//se o arquivo ja existe, substituir
+		// "201 CREATED\n"
+		// "404 NOT FOUND\n"
+		// "500 INTERNAL SERVER ERROR\n"
+		//receber e salvar o arquivo
 
+	} else if (strcmp(command, "DELETE") == 0) {
+		if (filename == NULL) {
+			strcpy(resposta, "400 Bad Request\n");
+		} else {
+			//apagar o arquivo
+			char path_file[200] = "servidor/";
+			strcat(path_file, filename);
 
-		const char delimiters[] = " \n";
-		char *command = strtok(requisicao, delimiters);
-		char *filename = strtok(NULL, delimiters);
-		char *timestamp = strtok(NULL, delimiters);
-		
-
-		if (strcmp(command, "GET") == 0) {
-					int fd = open(filename,O_RDONLY);
-
-			if (fd == -1){
-				strcpy(resposta, "400 Not Found\n");
-			}	
-			else{
-				struct stat st;
-				stat(filename, &st);
-				if (compare(st.st_mtime, time){
-					close(fd);
-				} else {
-					char buffer[128];
-					ssize_t bytesLidos = read(fd, buffer, sizeof(buffer)-1);
-					if (bytesLidos == -1){ 
-						perror("Erro ao ler o arquivo");
-						close(fd);
-						exit(1);
+			FILE *file = fopen(path_file, "r");
+			if (file != NULL) {
+				fclose(file);
+				if (remove(path_file) == 0) {
 					strcpy(resposta, "200 OK\n");
-					buffer[bytesLidos] = '\0';
-					close(fd);
-					strcpy(arquivo, buffer);	
-				}
-			}			
-		} 
-		else if (strcmp(command, "PUT") == 0) {
-			if(filename == NULL){
-				strcpy(resposta, "400 Bad Request\n");
-			} else {
-				char *old_filename = "cliente/";
-				strcat(old_filename, filename);
-				char path_file[200] = "servidor/";
-				strcat(path_file, filename);
-				if (rename(old_filename, path_file) == 0) {
-					strcpy(resposta, "201 CREATED\n");
 				} else {
 					strcpy(resposta, "500 INTERNAL SERVER ERROR\n");
 				}
-			}
-			
-			strcpy(resposta, "200 OK\n");
-			//se o arquivo ja existe, substituir
-			// "201 CREATED\n"
-			// "404 NOT FOUND\n"
-			// "500 INTERNAL SERVER ERROR\n"
-			//receber e salvar o arquivo
-
-		} 
-		else if (strcmp(command, "DELETE") == 0) {
-			if(filename == NULL){
-				strcpy(resposta, "400 Bad Request\n");
 			} else {
-				//apagar o arquivo
-				char path_file[200] = "servidor/";
-				strcat(path_file, filename);
-				
-				FILE* file = fopen(path_file, "r");
-				if(file != NULL){
-					fclose(file);
-					if (remove(path_file) == 0){
-						strcpy(resposta, "200 OK\n");
-					} else {
-						strcpy(resposta, "500 INTERNAL SERVER ERROR\n");
-					}
-				} else {
-					strcpy(resposta, "404 NOT FOUND\n");
-				}
+				strcpy(resposta, "404 NOT FOUND\n");
 			}
-		} 
-		else {
-			strcpy(resposta, "400 BAD REQUEST\n");
 		}
+	} else {
+		strcpy(resposta, "400 BAD REQUEST\n");
+	}
 
-		// send
-		ns = send(cfd, resposta, nr, 0);
-		if (ns < 0){
+	// send
+	ns = send(cfd, resposta, strlen(resposta), 0);
+	if (ns < 0) {
+		perror("erro no send()");
+		return NULL;
+	}
+	if (arquivo[0] != '\0') {
+		int ns2 = send(cfd, arquivo, strlen(arquivo), 0);
+		if (ns2 < 0) {
 			perror("erro no send()");
 			return NULL;
 		}
-		if(arquivo[0] == '\0'){
-			ns2 = send(cfd, arquivo, nr, 0);
-		}
-		// close
-		close(tclients[cfd].cfd);
+	}
+	// close
+	close(tclients[cfd].cfd);
 
-		tclients[cfd].cfd = -1;
-		return NULL;
+	tclients[cfd].cfd = -1;
+	return NULL;
 }
 
 int main(int argc, char **argv) {
@@ -190,4 +200,3 @@ int main(int argc, char **argv) {
 	}
 	return 0;
 }
- 
